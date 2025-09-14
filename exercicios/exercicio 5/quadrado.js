92fcf1ba05eac5604c5504a13c09fecc8b0b6dde
João Vitor Andrade <129785228+andradejao@users.noreply.github.com>
2025-09-14 09:29:13 -0300

    Delete exercicios/exercicio 5 directory

--- exercicios/exercicio 5/quadrado.js
+++
@@ -1,5 +0,0 @@
-function calcular() {
-    const valorLado = Number(document.getElementById('valor').value)
-    const valorArea = valorLado * valorLado
-    document.getElementById('mostrarValor').innerText = `A área do quadrado é ${valorArea}`
-}
